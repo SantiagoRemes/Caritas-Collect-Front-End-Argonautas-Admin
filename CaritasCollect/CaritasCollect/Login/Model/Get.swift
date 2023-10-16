d251cb6c4f9e26123698d28546a37c782ab32b45
SantiagoRemes <110937800+SantiagoRemes@users.noreply.github.com>
2023-10-16 13:31:52 -0700

    aaa

---
+++ CaritasCollect/CaritasCollect/Login/Model/Get.swift
@@ -0,0 +1,14 @@
+//
+//  Get.swift
+//  CaritasCollect
+//
+//  Created by Santiago Remes Inguanzo on 16/10/2023.
+//
+
+import Foundation
+
+struct Get : Codable {
+    var id: Int
+    var mensaje: String
+    var success: Bool
+}
